7a5c84af830364f9c5d3534569c4801b47a13d83
Aya Igarashi <Ladiclexxx@gmail.com>
2020-05-02 18:09:01 +0900

    FIx title

--- git-prompt.rb
+++ git-prompt.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class KubectlRolesum < Formula
+class GitPrompt < Formula
   desc "Analyze and print git status for prompt"
   homepage "https://github.com/Ladicle/git-prompt"
   url "https://github.com/Ladicle/git-prompt/releases/download/v1.0.0/git-prompt_darwin-amd64.tar.gz"
