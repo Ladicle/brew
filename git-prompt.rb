9ea75f355b8f9ecca3ed22d7388dd955c715d6e7
Aya Igarashi <aigarash@zlab.co.jp>
2020-05-02 15:36:26 +0900

    Add git-prompt

---
+++ git-prompt.rb
@@ -0,0 +1,11 @@
+require "formula"
+
+class KubectlRolesum < Formula
+  desc "Analyze and print git status for prompt"
+  homepage "https://github.com/Ladicle/git-prompt"
+  url "https://github.com/Ladicle/git-prompt/releases/download/v1.0.0/git-prompt_darwin-amd64.tar.gz"
+  sha256 "f371b767090e1c8e722846086ec6909452e14b6d76f4f975bf2c201e408ffcab"
+  def install
+    bin.install "git-prompt"
+  end
+end
